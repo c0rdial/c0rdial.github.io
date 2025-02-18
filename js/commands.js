9e9d941445ed53468975d312ee312f4eeb4d3fde
Adam <0132601@student.uow.edu.my>
2025-02-18 13:44:34 +0800

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -28,9 +28,9 @@ whoami = [
 social = [
     "<br>",
     'Twitch      <a href="' + twitch + '" target="_blank">twitch/cocacordial' + "</a>",
-    'Substack     <a href="' + substack + '" target="_blank"></a>', 
+    'Substack     <a href="' + substack + '" target="_blank">substack.com/@adamwan1' + </a>', 
     'Github      <a href="' + github + '" target="_blank">github/c0rdial' + "</a>",
-    'LinkTree     <a href="' + github + '" target="_blank">https://linktr.ee/wan4dam' + "</a>",
+    'LinkTree     <a href="' + github + '" target="_blank">linktr.ee/wan4dam' + "</a>",
     "<br>"
 ];
 
